4d29725e851ba37f2b71f8299f47f73d2d2f2c71
Sowmya Shree V N <sowmyagowdavn824@gmail.com>
2026-05-13 13:41:37 +0530

    Add files via upload

---
+++ sum of list elements without using function.py
@@ -0,0 +1,7 @@
+def summation(my_list):
+   sum = 0
+
+for i in my_list:
+    sum = sum + i
+
+print("Sum =", sum)
---
+++ sum(a,b).py
@@ -0,0 +1,6 @@
+def sum(a,b):
+    return a+b
+a=int(input("enter a"))
+b=int(input("enter b"))
+x=sum(a,b)
+print("sum=",x)
